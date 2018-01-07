885c5b40bc22a14ef54930c821e68aa60e29954e
StardustZiggy <khardlif1@gmail.com>
2018-01-07 11:19:46 +0100

    Add project files.

---
+++ StarVideoPlayer/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace StarVideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.buttonMinimize = new System.Windows.Forms.Button();
+            this.buttonMaximize = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(87)))), ((int)(((byte)(74)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(189, 729);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::StarVideoPlayer.Properties.Resources.video_player__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button12
+            // 
+            this.button12.FlatAppearance.BorderSize = 0;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button12.ForeColor = System.Drawing.Color.White;
+            this.button12.Location = new System.Drawing.Point(3, 393);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(189, 63);
+            this.button12.TabIndex = 4;
+            this.button12.Text = "     Favorites";
+            this.button12.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.FlatAppearance.BorderSize = 0;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button11.ForeColor = System.Drawing.Color.White;
+            this.button11.Location = new System.Drawing.Point(0, 324);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(189, 63);
+            this.button11.TabIndex = 3;
+            this.button11.Text = "     Recently Added";
+            this.button11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.FlatAppearance.BorderSize = 0;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button10.ForeColor = System.Drawing.Color.White;
+            this.button10.Location = new System.Drawing.Point(0, 255);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(189, 63);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "     Recently Played";
+            this.button10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button9.ForeColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(3, 186);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(189, 63);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "     Recently Viewed";
+            this.button9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button8.ForeColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(0, 117);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(189, 63);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "     Now Playing";
+            this.button8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(189, 650);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(819, 79);
+            this.panel2.TabIndex = 1;
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::StarVideoPlayer.Properties.Resources._003_stop_21;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.button7.Location = new System.Drawing.Point(257, 22);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(46, 45);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "\r\n";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::StarVideoPlayer.Properties.Resources._001_rewind;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.button6.Location = new System.Drawing.Point(321, 22);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(46, 45);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "\r\n";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::StarVideoPlayer.Properties.Resources._001_rewindRight;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.button5.Location = new System.Drawing.Point(454, 22);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(46, 45);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "\r\n";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::StarVideoPlayer.Properties.Resources._004_play_button_1;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.button4.Location = new System.Drawing.Point(386, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(46, 45);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "\r\n";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(48)))), ((int)(((byte)(47)))));
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(189, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(819, 650);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.panel4.Controls.Add(this.buttonMinimize);
+            this.panel4.Controls.Add(this.buttonMaximize);
+            this.panel4.Controls.Add(this.buttonExit);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(819, 40);
+            this.panel4.TabIndex = 0;
+            // 
+            // buttonMinimize
+            // 
+            this.buttonMinimize.BackgroundImage = global::StarVideoPlayer.Properties.Resources._002_minimize;
+            this.buttonMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonMinimize.FlatAppearance.BorderSize = 0;
+            this.buttonMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMinimize.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonMinimize.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.buttonMinimize.Location = new System.Drawing.Point(708, 4);
+            this.buttonMinimize.Name = "buttonMinimize";
+            this.buttonMinimize.Size = new System.Drawing.Size(29, 29);
+            this.buttonMinimize.TabIndex = 2;
+            this.buttonMinimize.Text = "\r\n";
+            this.buttonMinimize.UseVisualStyleBackColor = true;
+            this.buttonMinimize.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonMinimize_MouseClick);
+            // 
+            // buttonMaximize
+            // 
+            this.buttonMaximize.BackgroundImage = global::StarVideoPlayer.Properties.Resources._001_maximize;
+            this.buttonMaximize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonMaximize.FlatAppearance.BorderSize = 0;
+            this.buttonMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMaximize.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonMaximize.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.buttonMaximize.Location = new System.Drawing.Point(743, 4);
+            this.buttonMaximize.Name = "buttonMaximize";
+            this.buttonMaximize.Size = new System.Drawing.Size(29, 29);
+            this.buttonMaximize.TabIndex = 1;
+            this.buttonMaximize.Text = "\r\n";
+            this.buttonMaximize.UseVisualStyleBackColor = true;
+            this.buttonMaximize.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonMaximize_MouseClick);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackgroundImage = global::StarVideoPlayer.Properties.Resources._003_cancel;
+            this.buttonExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonExit.FlatAppearance.BorderSize = 0;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonExit.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.buttonExit.Location = new System.Drawing.Point(778, 4);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(29, 29);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "\r\n";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ButtonExit_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonMinimize;
+        private System.Windows.Forms.Button buttonMaximize;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ StarVideoPlayer/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StarVideoPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseApplication(Object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Minimize(Object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void ButtonExit_MouseClick(object sender, MouseEventArgs e)
+        {
+            CloseApplication(sender, e);
+        }
+
+        private void ButtonMaximize_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void ButtonMinimize_MouseClick(object sender, MouseEventArgs e)
+        {
+            Minimize(sender, e);
+        }
+
+
+    }
+}
